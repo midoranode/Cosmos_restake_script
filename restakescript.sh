c7baf0c70d71f91226e2e07cb03ad5727e7a8e1b
Tony <104720231+midoranode@users.noreply.github.com>
2023-10-29 10:03:17 +0800

    Update restakescript.sh

--- restakescript.sh
+++ restakescript.sh
@@ -3,7 +3,7 @@
 while true
     do
     
-        SERVICE=hid-noded
+        BIN=hid-noded
         VALOPER=hidvaloper****
         WALLETNAME=hyperwallet
         WALLETADDRESS=hid1****
@@ -15,15 +15,15 @@ while true
         NODEPORT=26657
         RESTAKETIME=21600
         
-        echo $PASSWORD | $SERVICE tx distribution withdraw-rewards $VALOPER --from $WALLETNAME --fees $FEES$DENOM --commission -y --chain-id $CHAINID --node tcp://localhost:$NODEPORT
+        echo $PASSWORD | $BIN tx distribution withdraw-rewards $VALOPER --from $WALLETNAME --fees $FEES$DENOM --commission -y --chain-id $CHAINID --node tcp://localhost:$NODEPORT
         sleep 30
-        BALANCE=$($SERVICE q bank balances $WALLETADDRESS --chain-id $CHAINID --node tcp://localhost:$NODEPORT --output json | jq -r ".balances[] | select(.denom == \"$DENOM\")  | .amount")
+        BALANCE=$($BIN q bank balances $WALLETADDRESS --chain-id $CHAINID --node tcp://localhost:$NODEPORT --output json | jq -r ".balances[] | select(.denom == \"$DENOM\")  | .amount")
         if [ "$BALANCE" -gt "$MINBAL" ] # or  (( $BALANCE > $MINBAL ))
          
             then
                 SUM=$(( $BALANCE - $MINBAL ))
                 sleep 3
-                echo $PASSWORD | $SERVICE tx staking delegate $VALOPER $SUM$DENOM --from $WALLETNAME --fees $FEES$DENOM -y --chain-id $CHAINID --node tcp://localhost:$NODEPORT
+                echo $PASSWORD | $BIN tx staking delegate $VALOPER $SUM$DENOM --from $WALLETNAME --fees $FEES$DENOM -y --chain-id $CHAINID --node tcp://localhost:$NODEPORT
                 sleep $RESTAKETIME
             
             else
